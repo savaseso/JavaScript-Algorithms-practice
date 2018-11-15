0a011a49ab38021d5927521d0b265f1262f1ed93
Bence Rigler <rigbence@gmail.com>
2018-11-15 02:25:34 +0100

    increasing Sequence

---
+++ almostIncreasingSequence.js
@@ -0,0 +1,20 @@
+//Given a sequence of integers as an array, determine whether it is possible to obtain a strictly increasing sequence by removing no more than one element from the array.
+
+/*example: 
+[1,3,2,1] -->false
+[5,6,3,9] -->true
+
+*/
+
+function almostIncreasingSequence(arr){
+    let count = 0;
+    for(let i=0 ; i<arr.length ; i++){
+        if(arr[i]>arr[i+1]){
+            count++;
+        }
+    }
+   if (count<=1){
+       return true
+   }
+   return false
+}
