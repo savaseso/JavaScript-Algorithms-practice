7904f548442441ca0b12b7677ba5fb8aa8dd4f06
Bence Rigler <rigbence@gmail.com>
2019-02-25 20:34:47 +0100

    finding the number of vowels in a string

---
+++ vowels.js
@@ -0,0 +1,13 @@
+//return the number of vowels in a string
+
+
+function vowels (str) {
+    let count = 0;
+    const checker = ['a','e','i','o','u'];
+    for(let char of str.toLowerCase()){
+        if(checker.includes(char)){
+            count++;
+        }
+    }
+    return count;
+}
