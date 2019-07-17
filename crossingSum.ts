eafcb60760acf3f2e4d223295b90c2ad0af1a4b3
Bence Rigler <rigbence@gmail.com>
2019-07-17 19:07:20 +0200

    Add files via upload

---
+++ crossingSum.ts
@@ -0,0 +1,15 @@
+const crossingSum = (matrix:array,a:number,b:number):number => {
+    const columnTotal = matrix[a].reduce((a, b) => a+b);
+    let rowTotal = 0;
+
+    for(let i = 0 ; i < matrix.length ; i++){
+        rowTotal += i !== a ? matrix[i][b] : 0;
+    }
+
+    return columnTotal + rowTotal
+}
+
+console.log([[1, 1, 1, 1],
+    [2, 2, 2, 2],
+    [3, 3, 3, 3],
+    ],1,3)
