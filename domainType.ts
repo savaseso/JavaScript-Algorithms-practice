31024e7241feaabda535afd589b3e7764125398f
Bence Rigler <rigbence@gmail.com>
2019-07-17 21:30:25 +0200

    Add files via upload

---
+++ domainType.ts
@@ -0,0 +1,23 @@
+const domainType = (arr) => {
+ 
+    let domainArray = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        let ending = arr[i].substring(arr[i].length - 3);
+        switch(ending){
+            case 'org':
+            domainArray.push('organization');
+            break;
+            case 'com':
+            domainArray.push('commercial');
+            break;
+            case 'net':
+            domainArray.push('network');
+            break;
+            case 'nfo':
+            domainArray.push('information');
+            default:
+            break;
+        }
+    }
+    return domainArray;
