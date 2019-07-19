f9488de073a00a8ca37ffcab901a8172c6d9eb31
Bence Rigler <rigbence@gmail.com>
2019-07-19 17:06:49 +0200

    Add files via upload

---
+++ fermactor.ts
@@ -0,0 +1,11 @@
+const fermactor = (n: number): number [] => {
+    for(let i = 1 ; i < n; i++){
+        for(let j = 1; j < i ; j++){
+            const total = Math.pow(i,2) - Math.pow(j,2);
+
+            if(total === n){
+                return [i, j]
+            }
+        }
+    }
+}
