08c842ee2b71b41d009e0a8bd6610b835b8b8235
Bence Rigler <rigbence@gmail.com>
2019-07-23 00:44:37 +0200

    Add files via upload

---
+++ houseNumbersSum.tsx
@@ -0,0 +1,13 @@
+
+const houseNumbersSum = (inputArray:number[]):number =>{
+    let total:number = 0;
+    for(let i = 0 ; i<inputArray.length ; i++){
+        if(inputArray[i] === 0){
+           return total
+        }
+        total += inputArray[i]
+    }
+  
+}
+
+console.log(houseNumbersSum([5,1,2,3,0,1,5,0,2]))
