68d5a07ba00a7478cd0e2295851709039b6a6d18
Bence Rigler <rigbence@gmail.com>
2019-07-24 16:42:17 +0200

    Add files via upload

---
+++ isLucky.ts
@@ -0,0 +1,16 @@
+const isLucky = (n:number):boolean =>{
+    const luckyN = n.toString().split('')
+    const halfWay = luckyN.length/2
+    const firstHalf = luckyN.slice(0,halfWay)
+    const secondHalf = luckyN.slice(halfWay,luckyN.length)
+    const firstNumber = firstHalf.reduce((total,element) => {
+        return parseInt(element) + total
+    },0)
+    const secondNumber = secondHalf.reduce((total,element)=>{
+        return parseInt(element) + total
+    },0)
+    return firstNumber === secondNumber
+}
+console.log(isLucky(1230)) //true
+console.log(isLucky(239017))//false
+console.log(isLucky(1661))//true
