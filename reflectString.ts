5948c11a2016fb6516383a9d595b2fe8a1045ad0
Bence Rigler <rigbence@gmail.com>
2019-07-28 20:38:11 +0200

    Add files via upload

---
+++ reflectString.ts
@@ -0,0 +1,13 @@
+const reflectString = (inputString:string) =>{
+    let reflectedWord = ''
+    let alphabet = {
+        'a': 'z', 'b': 'y', 'c': 'x', 'd': 'w', 'e': 'v', 'f': 'u', 'g': 't', 'h': 's', 'i': 'r', 'j': 'q', 'k': 'p', 'l': 'o', 'm': 'n', 
+        'n': 'm', 'o': 'l', 'p': 'k', 'q': 'j', 'r': 'i', 's': 'h', 't': 'g', 'u': 'f', 'v': 'e', 'w': 'd', 'x': 'c', 'y': 'b', 'z': 'a'
+    };
+    for(let char of inputString){
+        reflectedWord += alphabet[char]
+    }
+    return reflectedWord
+ }
+
+console.log(reflectString('name'))
